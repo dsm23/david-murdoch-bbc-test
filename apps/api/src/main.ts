e85c1bfe466d716f431ff99e88402ec9ba610da8
dsm23 <dsmurdoch357@gmail.com>
2019-10-08 21:58:29 +0100

    Update all remaining packages

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -22,10 +22,3 @@ async function bootstrap() {
 }
 
 bootstrap();
-
-export const handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: 'Hello, World',
-  });
-};
